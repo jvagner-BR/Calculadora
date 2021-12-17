61f20e1c467e7a172fb1da097e96a2e4f0f5687d
João Vagner <83413866+jvagner-BR@users.noreply.github.com>
2021-12-17 19:03:06 -0300

    Commit #2
    updating the site

--- script.js
+++ script.js
@@ -12,6 +12,13 @@ function back()
     var resultado = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
 }
+function percent() 
+{
+    var pPos = $('#pointspossible'). val();
+    var pEarned = $('#pointsgiven'). val();
+    var perc = ((pEarned/pPos) * 100).toFixed(3);
+    $('#pointsperc').val(perc);
+}
 function calcular()
 {
     var resultado = document.getElementById('resultado').innerHTML;
